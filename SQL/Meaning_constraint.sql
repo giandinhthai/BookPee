1e5f2f84981dc096ec8a5150fe6ace8c8640506b
hoaitrungchar <79318706+hoaitrungchar@users.noreply.github.com>
2023-12-05 00:55:19 +0700

    update constraint

--- SQL/Meaning_constraint.sql
+++ SQL/Meaning_constraint.sql
@@ -53,16 +53,169 @@ BEGIN
     inner join discount p2 on p1.genres=p2.genres
     where book_id=new.book_id and discount_id=new.discount_id)
     
--- 	With discount_genres As
---     (Select genres 
---     from discount
---     where discount_id=new.discount_id)
+    Select Count(*)
+    Into is_having_genres 
+    from book_genres;
+    
+    If (is_having_genres<=0) then
+		signal sqlstate '45000' set message_text ='Sách không thuộc thể loại của chương trình khuyến mãi này';
+    end if;
+END;
+|
+Create Trigger assign_db.have_match_genres_update
+before update on assign_db.have_
+for each row	
+BEGIN
+	Declare is_having_genres INT default 0;
+    
+    With book_genres As
+    (Select book_id
+    From genres_book p1
+    inner join discount p2 on p1.genres=p2.genres
+    where book_id=new.book_id and discount_id=new.discount_id)
     
     Select Count(*)
     Into is_having_genres 
     from book_genres;
     
-    
-END;
+    If (is_having_genres<=0) then
+		signal sqlstate '45000' set message_text ='Sách không thuộc thể loại của chương trình khuyến mãi này';
+    end if;
+END
 |
+Create Trigger assign_db.adult_age_16_insert
+before insert on assign_db.adult
+for each row	
+BEGIN
+	Declare customer_birthday date;
+    Declare age INT default 0;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    If (age<16) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản trưởng thành phải lớn hơn bằng 16';
+    end if;
+END
+|
+
+Create Trigger assign_db.adult_age_16_update
+before update on assign_db.adult
+for each row	
+BEGIN
+	Declare customer_birthday date;
+    Declare age INT default 0;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    If (age<16) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản trưởng thành phải lớn hơn bằng 16';
+    end if;
+END
+|
+
+Create Trigger assign_db.adult_child_16_insert
+before insert on assign_db.child
+for each row	
+BEGIN
+	Declare customer_birthday date;
+    Declare age INT default 0;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    If (age>16) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản trẻ em phải bé hơn 16';
+    end if;
+END
+|
+
+Create Trigger assign_db.child_age_16_update
+before update on assign_db.child
+for each row	
+BEGIN
+	Declare customer_birthday date ;
+    Declare age INT default 0;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    If (age>=16) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản trẻ em phải bé hơn 16';
+    end if;
+END
+-- |
+-- Create Trigger assign_db.apply_for_membership_match
+-- before update on assign_db.apply_for
+-- for each row	
+-- BEGIN
+-- 	Declare membership_customer INT ;
+--     Declare membership_promotion INT;
+--     
+--     Select membership
+--     from  promotion_code
+--     into membership_customer;
+--     
+--     
+--     If (age>=16) then
+-- 		signal sqlstate '45000' set message_text ='Tuổi của tài khoản trẻ em phải bé hơn 16';
+--     end if;
+-- END
+|
+Create Trigger assign_db.contain_age
+before insert on assign_db.contain
+for each row	
+BEGIN
+	Declare customer_id_of_order int;
+	Declare customer_birthday date ;
+    Declare age INT default 0;
+    Declare book_reading_age int;
+    
+    Select customer_id into customer_id_of_order
+    From order_
+    where order_.order_id=new.order_id;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    
+    Select reading_age
+    into book_reading_age
+    From book 
+    Where book.book_id=new.book_id;
+    
+    If (age<book_reading_age) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản không đủ để đọc cuốn sách này';
+    end if;
+END
+|
+Create Trigger assign_db.contain_age
+before update on assign_db.contain
+for each row	
+BEGIN
+	Declare customer_id_of_order int;
+	Declare customer_birthday date ;
+    Declare age INT default 0;
+    Declare book_reading_age int;
+    
+    Select customer_id into customer_id_of_order
+    From order_
+    where order_.order_id=new.order_id;
+	SELECT birthday into customer_birthday
+	FROM customer
+	WHERE customer_id=new.customer_id;
+	SELECT TIMESTAMPDIFF(YEAR, customer_birthday, CURDATE()) into age;
+    
+    Select reading_age
+    into book_reading_age
+    From book 
+    Where book.book_id=new.book_id;
+    
+    If (age<book_reading_age) then
+		signal sqlstate '45000' set message_text ='Tuổi của tài khoản không đủ để đọc cuốn sách này';
+    end if;
+END
+|
+
 DELIMITER ;
